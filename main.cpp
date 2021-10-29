e309ead5b7aae78fce9f57fe27124c799315b7e4
Kim Sung Hyun <soakdma37@gmail.com>
2021-10-30 01:25:09 +0900

    insertion sort 구현 완료

--- main.cpp
+++ main.cpp
@@ -12,43 +12,43 @@ typedef struct {
 	char comments[16];
 }ELEMENT;
 
-typedef int _Cmpfun(const void *, const void *);
+typedef int _Cmpfun(const void*, const void*);
 
 void copy_element(ELEMENT* dist, ELEMENT* source) {
 	int i;
-	dist->score=source->score;
-	for(i=0; i<ELEMENT_DATA_LENGTH; i++) {
-		dist->data[i]=source->data[i];
+	dist->score = source->score;
+	for (i = 0; i < ELEMENT_DATA_LENGTH; i++) {
+		dist->data[i] = source->data[i];
 	}
-	for(i=0; i<ELEMENT_COMMENT_LENGTH; i++) {
-		dist->comments[i]=source->comments[i];
+	for (i = 0; i < ELEMENT_COMMENT_LENGTH; i++) {
+		dist->comments[i] = source->comments[i];
 	}
 }
 
 void init_score_array(unsigned int* data, int n) {
 	int i, j;
 	unsigned int tmp;
-	for(i=0; i<n; i++) {
-		data[i]=i;
+	for (i = 0; i < n; i++) {
+		data[i] = i;
 	}
 
 	srand((unsigned int)time(NULL));
 
-	for(i = n - 1; i >= 1; i--) {
+	for (i = n - 1; i >= 1; i--) {
 		j = rand() % (i + 1);
 
 		tmp = data[i]; data[i] = data[j]; data[j] = tmp;
 	}
 }
 
-int my_element_score_compare(const void *a, const void *b) {
-	ELEMENT *elem_a, *elem_b;
-	elem_a=(ELEMENT*)a;
-	elem_b=(ELEMENT*)b;
-	if(elem_a->score==elem_b->score) {
+int my_element_score_compare(const void* a, const void* b) {
+	ELEMENT* elem_a, * elem_b;
+	elem_a = (ELEMENT*)a;
+	elem_b = (ELEMENT*)b;
+	if (elem_a->score == elem_b->score) {
 		return 0;
 	}
-	else if((elem_a->score)<(elem_b->score)) {
+	else if ((elem_a->score) < (elem_b->score)) {
 		//뒤쪽이 더 크면 -1
 		return -1;
 	}
@@ -59,65 +59,79 @@ int my_element_score_compare(const void *a, const void *b) {
 
 void qsort_orig(void* arr, size_t n, size_t size, _Cmpfun* cmp) {
 	//printf("%d ", n);
-	if(n>1) {
+	if (n > 1) {
 		int i, j;
-		ELEMENT* pivot=(ELEMENT*)arr;
-		ELEMENT* array_pointer=(ELEMENT*)arr;
-		//맨 왼쪽 인덱스의 점수를 따 온다
-		int pivot_position=0;
-		for(i=0; i<n; i++) {
-			if(my_element_score_compare(array_pointer+i, pivot)==-1) {
-				pivot_position++;
-				//pivot보다 더 작은 것들의 수를 센다.
-				//pivot보다 크거나 같은건 right에 들어갈것
-			}
-		}
-		ELEMENT* left_array=(ELEMENT*)malloc(size*pivot_position);
-		ELEMENT* right_array=(ELEMENT*)malloc(size*(n-pivot_position));
-		//pivot 위치는 left, right array의 사이
-		//printf("%d\n", pivot_position);
-		int left_cursor=0, right_cursor=0;
-		for(i=0; i<n; i++) {
-			//pivot을 기준으로 작은 배열, 큰 배열 나눔
-			if(my_element_score_compare(array_pointer+i, pivot)==-1) {
-				memcpy(left_array+left_cursor, array_pointer+i, size);
-				left_cursor++;
-			}
-			else {
-				memcpy(right_array+right_cursor, array_pointer+i, size);
-				right_cursor++;
+		ELEMENT* pivot_pointer = (ELEMENT*)arr;
+		ELEMENT* array_pointer = (ELEMENT*)arr;
+		ELEMENT* buf = (ELEMENT*)malloc(size);
+		ELEMENT* smaller_than_pivot_pointer = (ELEMENT*)arr;
+		int smaller_count = 0;
+		//pivot보다 작은 원소 개수를 센다
+
+		memcpy(buf, pivot_pointer, size);
+		memcpy(pivot_pointer, array_pointer + n - 1, size);
+		memcpy(array_pointer + n - 1, buf, size);
+		//첫 원소에 있던 피벗을 맨 끝으로 보낸다
+
+		pivot_pointer = array_pointer + n - 1;
+		//이제 맨 끝에 피벗이 있다
+		for (i = 0; i < n; i++) {
+			//현재 확인하는 원소는 i번째 인덱스. 만약 이게 피벗보다 작으면 smaller_than_pivot_pointer 위치로 보낸디
+			if (cmp(array_pointer + i, pivot_pointer) == -1) {
+				memcpy(buf, array_pointer + i, size);
+				memcpy(array_pointer + i, smaller_than_pivot_pointer, size);
+				memcpy(smaller_than_pivot_pointer, buf, size);
+				smaller_than_pivot_pointer++;
+				smaller_count++;
 			}
 		}
 
-		memcpy(array_pointer, left_array, size*pivot_position);
-		memcpy(array_pointer+pivot_position, right_array, size*(n-pivot_position));
+		memcpy(buf, pivot_pointer, size);
+		memcpy(pivot_pointer, smaller_than_pivot_pointer, size);
+		memcpy(smaller_than_pivot_pointer, buf, size);
 
-		qsort_orig(array_pointer, pivot_position, size, cmp);
-		qsort_orig(array_pointer+pivot_position+1, n-pivot_position-1, size, cmp);
+		qsort_orig(array_pointer, smaller_count, size, cmp);
+		qsort_orig(array_pointer + smaller_count + 1, n - smaller_count - 1, size, cmp);
 	}
 
 }
 
-void insertion_sort(void* arr, int n);
+void insertion_sort(void* arr, size_t n, size_t size, _Cmpfun* cmp) {
+	ELEMENT* array_pointer = (ELEMENT*)arr;
+	int i, j;
+	ELEMENT* buf = (ELEMENT*)malloc(size);
+
+	for (i = 1; i < n; i++) {
+		memcpy(buf, array_pointer + i, size);
+		j = i;
+		while ((j > 0) && cmp(buf, array_pointer + j - 1)==-1) {
+			memcpy(array_pointer + j, array_pointer + j - 1, size);
+			j--;
+		}
+		memcpy(array_pointer + j, buf, size);
+	}
+}
 
 void qsort_median_insert(void*, size_t, size_t, _Cmpfun*);
 void qsort_median_insert_iter(void*, size_t, size_t, _Cmpfun*);
 
 int main() {
-	int i, j;
-	unsigned int arr[105];
-	init_score_array(arr, 50);
-	for(i=0; i<50; i++) {
-		printf("%d\n", arr[i]);
-	}
-	printf("\n");
 
-	ELEMENT unsorted_list[105];
-	for(i=0; i<50; i++) {
-		unsorted_list[i].score=arr[i];
+	int n;
+	int i, j;
+	int arr[1005];
+	//init_score_array(arr, 10);
+	scanf("%d", &n);
+	for (i = 0; i < n; i++) {
+		scanf("%d", arr+i);
 	}
-	qsort_orig(unsorted_list, 50, sizeof(ELEMENT), my_element_score_compare);
-	for(i=0; i<50; i++) {
+
+	ELEMENT unsorted_list[1005];
+	for (i = 0; i < n; i++) {
+		unsorted_list[i].score = arr[i];
+	}
+	insertion_sort(unsorted_list, n, sizeof(ELEMENT), my_element_score_compare);
+	for (i = 0; i < n; i++) {
 		printf("%d\n", unsorted_list[i].score);
 	}
 
